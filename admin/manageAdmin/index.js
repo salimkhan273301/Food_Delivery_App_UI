f7248df11e6e1864c6f958272f3cc6ba00b46cb0
salimkhan273301 <ajadi273301@gmail.com>
2023-08-08 22:47:00 +0530

    used two diff modal for registration and updation

--- admin/manageAdmin/index.js
+++ admin/manageAdmin/index.js
@@ -26,8 +26,8 @@ function populateTable(response) {
 
         <td>${admin.password}</td>
         <td>
-            <button class="btn btn-primary btn-sm view-customer-btn" data-customer-view="${admin.customerId}" onclick="viewCustomer(${admin.adminId})"><i class="fas fa-eye"></i>View</button>
-            <button class="btn btn-info btn-sm edit-admin-btn" data-admin-edit="${admin.adminId}" onclick="editadmin(${admin.adminId})"><i class="fas fa-edit"></i>Edit</button>
+            <button class="btn btn-primary btn-sm view-admin-btn" data-admin-view="${admin.adminId}" onclick="viewadmin(${admin.adminId})"><i class="fas fa-eye"></i>View</button>
+            <button class="btn btn-info btn-sm edit-admin-btn" data-admin-edit="${admin.adminId}" onclick="editAdmin(${admin.adminId})"><i class="fas fa-edit"></i>Edit</button>
             <button class="btn btn-danger btn-sm delete-admin-btn" data-admin-delete="${admin.adminId}" onclick="deleteadmin(${admin.adminId})"><i class="fas fa-trash-alt"></i> Delete</button>
         </td>
       </tr>`;
@@ -39,11 +39,11 @@ fetchAdminProfile();
 
 function getDataFromRegistrationForm() {
   return {
-    name: $("#name"),
-    mobileNumber: $("#mobileNumber"),
-    address: $("#address"),
-    email: $("#email"),
-    password: $("#password"),
+    name: $("#name").val(),
+    mobileNumber: $("#mobileNumber").val(),
+    address: $("#address").val(),
+    email: $("#email").val(),
+    password: $("#password").val(),
   };
 }
 
@@ -70,3 +70,69 @@ $("#registerAdminBtn").click(function (e) {
   e.preventDefault();
   registerAdmin();
 });
+
+function editAdmin(adminId) {
+  const onSuccess = function (admin) {
+    populateForm(admin);
+    $("#editModal").modal("show");
+  };
+  const onError = function (error) {
+    console.error("Error fetching admin details:", error);
+  };
+
+  ajaxService(
+    `/admins/${adminId}`,
+    "GET",
+    undefined,
+    onSuccess,
+    onError,
+    `[data-admin-edit=${adminId}]`
+  );
+}
+
+function populateForm(admin) {
+  console.log(admin);
+  $("#editAdminId").val(admin.adminId);
+  $("#editName").val(admin.name);
+  $("#editMobileNumber").val(admin.phoneNumber);
+  $("#editAddress").val(admin.address);
+  $("#editEmail").val(admin.email);
+  $("#editPassword").val(admin.password);
+  // Populate password field
+}
+
+$("#saveChanges").on("click", function (event) {
+  event.preventDefault();
+  const adminId = $("#editAdminId").val();
+  const updatedData = {
+    name: $("#editName").val(),
+    mobileNumber: $("#editMobileNumber").val(),
+    address: $("#editAddress").val(),
+    email: $("#editEmail").val(),
+    password: $("#editPassword").val(),
+  };
+
+  updateadmin(adminId, updatedData);
+  console.log(updatedData);
+});
+
+function updateadmin(adminId, updatedData) {
+  const onSuccess = function (response) {
+    console.log("admin with ID " + adminId + " updated successfully");
+    $("#editModal").modal("hide");
+    // Refresh the table after updating the admin
+    fetchadmins();
+  };
+  const onError = function (error) {
+    console.error("Error updating admin:", error);
+  };
+
+  ajaxService(
+    `/admins/${adminId}`,
+    "PUT",
+    updatedData,
+    onSuccess,
+    onError,
+    "#saveChanges"
+  );
+}
